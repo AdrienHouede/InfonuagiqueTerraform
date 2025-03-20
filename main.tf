5d33d6b5fda79031df8a5880c511766e2ec58220
AdrienHouede <adrien.houede@gmail.com>
2025-03-20 18:29:35 +0100

    main tf modif

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ resource "azurerm_network_security_group" "adrien" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
 
 resource "azurerm_network_interface_security_group_association" "adrien" {
   network_interface_id      = azurerm_network_interface.adrien.id
